6bc957064dd0dd12ad5946b12423544f0ee82e7f
Jasson Rodríguez <jassonrm@icloud.com>
2021-03-28 23:11:35 -0600

    Add remaining requests

--- webserver/server.py
+++ webserver/server.py
@@ -11,12 +11,29 @@ class Server(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
+        print("GET")
         if self.path == "/":
-            self.path = "index.html"
+            file = open("index.html", mode='rb')
+            data = file.read()
+            file.close()
+            self.do_HEAD()
+            self.wfile.write(data)
         elif self.path == "/api/sensors":
             sensors = open("sensors.json", mode="rb")
             data = sensors.read()
             sensors.close()
+            self.do_HEAD()
+            self.wfile.write(data)
+        elif self.path == "/api/lights":
+            lights = open("lights.json", mode="rb")
+            data = lights.read()
+            lights.close()
+            self.do_HEAD()
+            self.wfile.write(data)
+        elif self.path == "/api/take_picture":
+            take_picture = open("take_picture.json", mode="rb")
+            data = take_picture.read()
+            take_picture.close()
             self.do_HEAD()
             self.wfile.write(data)
         elif self.path == "/api/picture":
@@ -43,6 +60,14 @@ class Server(BaseHTTPRequestHandler):
             sensors = open("sensors.json", mode="w")
             sensors.write(post_data.decode("utf-8"))
             sensors.close()
+        elif self.path == "/api/lights":
+            lights = open("lights.json", mode="w")
+            lights.write(post_data.decode("utf-8"))
+            lights.close()
+        elif self.path == "/api/take_picture":
+            take_picture = open("take_picture.json", mode="w")
+            take_picture.write(post_data.decode("utf-8"))
+            take_picture.close()
         elif self.path == "/api/picture":
             picture = open("picture.jpg", mode="wb")
             picture.write(post_data)
