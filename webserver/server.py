4754cc8380696562e95bd782e18931f91112b3e7
Marco <m.herrera0799@gmail.com>
2021-04-02 15:57:41 -0600

    draft

--- webserver/server.py
+++ webserver/server.py
@@ -1,13 +1,32 @@
+import json
+import mimetypes
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from uuid import uuid4
 
 host_port = 8000
+
+
+def save_token(token):
+    path = "tokens.json"
+    file = open(path, mode="r+")
+    tokens = json.loads(file.read())
+    tokens.append(token)
+    file.seek(0)
+    file.truncate()
+    json.dump(tokens, file)
+    file.close()
 
 
 class Server(BaseHTTPRequestHandler):
 
     def do_HEAD(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        mimetype, _ = mimetypes.guess_type(self.path)
+        self.send_header('Content-type', mimetype)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+        self.send_header("Access-Control-Allow-Headers", "X-Requested-With")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
         self.end_headers()
 
     def do_GET(self):
@@ -36,6 +55,12 @@ class Server(BaseHTTPRequestHandler):
             self.receive_file("lights.json", post_data)
         elif self.path == "/api/take_picture":
             self.receive_file("take_picture.json", post_data)
+        elif self.path == "/api/auth":
+            self.authenticate("user_db.json", post_data)
+        elif self.path == "/api/sign":
+            self.sign_up("user_db.json", post_data)
+        elif self.path == "/api/session":
+            self.validate_session("tokens.json", post_data)
         else:
             self.resource_not_found()
 
@@ -49,6 +74,10 @@ class Server(BaseHTTPRequestHandler):
             self.receive_file("picture.jpg", put_data)
         else:
             self.resource_not_found()
+
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.do_HEAD()
 
     def send_file(self, path):
         try:
@@ -65,6 +94,47 @@ class Server(BaseHTTPRequestHandler):
         picture.write(data)
         picture.close()
         self.do_HEAD()
+
+    def authenticate(self, path, data):
+        file = open(path, mode='r+')
+        users = json.loads(file.read())
+        data = json.loads(data)
+        for user in users:
+            if user["username"] == data["username"] and user["password"] == data["password"]:
+                token = str(uuid4())
+                save_token(token)
+                self.send_token(token)
+                return
+        token = str(0)
+        self.send_token(token)
+
+    def sign_up(self, path, data):
+        file = open(path, mode='r+')
+        users = json.loads(file.read())
+        data = json.loads(data)
+        users.append(data)
+        file.seek(0)
+        file.truncate()
+        json.dump(users, file)
+        file.close()
+
+    def validate_session(self, path, data):
+        file = open(path, mode='r+')
+        tokens = json.loads(file.read())
+        data = json.loads(data)
+        value = "invalid"
+        for token in tokens:
+            if token == data["token"]:
+                value = "valid"
+                break
+        self.do_HEAD()
+        self.wfile.write(json.dumps({"token": value}).encode())
+        file.close()
+
+    def send_token(self, token):
+        self.send_response(200)
+        self.send_header("Authorization", token)
+        self.end_headers()
 
     def resource_not_found(self):
         self.send_response(404)
---
+++ webserver/website/js/model.js
@@ -0,0 +1,83 @@
+const light_pins = {
+    "living-room-light": "pin 1",
+    "kitchen-light": "pin 2",
+    "dinning-room-light": "pin 3",
+    "room1-light": "pin 4",
+    "room2-light": "pin 5"
+}
+
+const light_states = {
+    "living-room-light": "0",
+    "kitchen-light": "0",
+    "dinning-room-light": "0",
+    "room1-light": "0",
+    "room2-light": "0"
+}
+
+const lights_ON = "yellow";
+const lights_OFF = "black";
+
+function get_server_light_state() {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'text';
+    xhr.open("GET", lights_url, true);
+    xhr.onreadystatechange = function () {
+        // Request finished. Do processing here.
+        const response = JSON.parse(xhr.responseText)
+        for (const light_id in light_pins) {
+            const pin = light_pins[light_id];
+            light_states[light_id] = response[pin]
+        }
+        update_lights();
+    };
+    xhr.send(null);
+}
+
+function set_server_light_state() {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.open("POST", lights_url, true);
+    const request = {}
+    for (const light_id in light_pins) {
+        const pin = light_pins[light_id];
+        request[pin] = light_states[light_id]
+    }
+    xhr.send(JSON.stringify(request));
+}
+
+function switch_light_state(light_id) {
+    if (light_id in light_states) {
+        light_states[light_id] = light_states[light_id] === "1" ? "0" : "1";
+        update_lights();
+        set_server_light_state();
+    }
+}
+
+function switch_all_lights(light_id) {
+    if (light_id in light_states) {
+        const state = light_states[light_id] === "1" ? "0" : "1";
+
+        for (const light_id in light_states) {
+            light_states[light_id] = state;
+        }
+        update_lights();
+        set_server_light_state();
+    }
+}
+
+function update_lights() {
+    for (const light_id in light_states) {
+        const light = document.getElementById(light_id);
+        console.log(JSON.stringify(light_states));
+        if (light_states[light_id] === "1") {
+            light.style.backgroundColor = lights_ON;
+        } else {
+            light.style.backgroundColor = lights_OFF;
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    validate_session(model_url, login_url);
+    get_server_light_state();
+})
