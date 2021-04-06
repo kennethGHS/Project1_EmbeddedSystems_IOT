3fb73b78575c65bdad61247d58e1c5579dbd3912
Jasson Rodriguez <jassonrm@icloud.com>
2021-04-06 07:35:33 -0600

    Remove gpio test from main

--- C-Code/src/main.c
+++ C-Code/src/main.c
@@ -2,15 +2,7 @@
 #include <stdio.h>
 #include "ManualGPIO.h"
 #include "Monitoring.h"
-int test_GPIO_input(){
-    int array_gpio[] = {23,24};
-    initialize_values(array_gpio,2);
-    char a_word[100];
-    printf ("Press something then enter to finish ");
-    scanf ("%s", a_word);
-    printf ("finishing");
-    destroy_monitoring();
-}
+
 int main(int argc, char const *argv[]) {
     int gpio_in[] = {14,15,18,23};
     int gpio_out[] = {2,3,4,17,27};
