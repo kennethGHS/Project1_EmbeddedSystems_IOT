c9f9379d3a6a6cec37763f496c3ec5c8afc6fc40
Jasson Rodriguez <jassonrm@icloud.com>
2021-04-09 20:46:51 -0600

    Change used pins

--- C-Code/src/main.c
+++ C-Code/src/main.c
@@ -4,8 +4,8 @@
 #include "Monitoring.h"
 
 int main(int argc, char const *argv[]) {
-    int gpio_in[] = {14,15,18,23};
-    int gpio_out[] = {2,3,4,17,27};
+    int gpio_in[] = {14,18,23,24};
+    int gpio_out[] = {2,3,4,17,22};
     initialize_values(gpio_in,4,gpio_out,5);
 	return 0;
 }
