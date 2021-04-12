bb10bc27c8e914f17f45399f7d97bda47fbd3bab
Jasson Rodriguez <jassonrm@icloud.com>
2021-04-11 22:00:03 -0600

    Add docstring

--- webserver/server.py
+++ webserver/server.py
@@ -7,6 +7,8 @@ host_port = 8000
 
 
 def save_token(token):
+    """This function saves a session token to disk"""
+
     path = "tokens.json"
     file = open(path, mode="r+")
     tokens = json.loads(file.read())
@@ -18,8 +20,11 @@ def save_token(token):
 
 
 class Server(BaseHTTPRequestHandler):
+    '''HTTP server with a RESTful API'''
 
     def do_HEAD(self):
+        """This method generates the header for every request path"""
+
         self.send_response(200)
         mimetype, _ = mimetypes.guess_type(self.path)
         self.send_header('Content-type', mimetype)
@@ -30,6 +35,8 @@ class Server(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
+        """This method resolves GET requests to the server, whether its for the API or a website"""
+
         path = self.path.split("?")[0]
         if self.path == "/":
             self.send_file("website/common/login.html")
@@ -45,6 +52,8 @@ class Server(BaseHTTPRequestHandler):
             self.send_file("website" + self.path)
 
     def do_POST(self):
+        """This method resolves POST requests to the API"""
+
         # Get the size of data
         content_length = int(self.headers['Content-Length'])
         # Get the data
@@ -66,6 +75,8 @@ class Server(BaseHTTPRequestHandler):
             self.resource_not_found()
 
     def do_PUT(self):
+        """This method resolves PUT requests to the API"""
+
         # Get the size of data
         content_length = int(self.headers['Content-Length'])
         # Get the data
@@ -80,9 +91,12 @@ class Server(BaseHTTPRequestHandler):
             self.resource_not_found()
 
     def do_OPTIONS(self):
+        """This method resolves OPTIONS requests to any path"""
         self.do_HEAD()
 
     def send_file(self, path):
+        """This method reads a file from disk and sends it as response of a request"""
+
         try:
             file = open(path, mode='rb')
             data = file.read()
@@ -93,12 +107,16 @@ class Server(BaseHTTPRequestHandler):
             self.resource_not_found()
 
     def receive_file(self, path, data):
+        """This method write data received as a request to a file on disk"""
+
         file = open(path, mode="wb")
         file.write(data)
         file.close()
         self.do_HEAD()
 
     def authenticate(self, path, data):
+        """This method authenticates a user given the username and password"""
+
         file = open(path, mode='r+')
         users = json.loads(file.read())
         data = json.loads(data)
@@ -112,6 +130,8 @@ class Server(BaseHTTPRequestHandler):
         self.send_token(token)
 
     def sign_up(self, path, data):
+        """This method registers a new user and grants permissions to use the platform"""
+
         file = open(path, mode='r+')
         users = json.loads(file.read())
         data = json.loads(data)
@@ -122,6 +142,8 @@ class Server(BaseHTTPRequestHandler):
         file.close()
 
     def validate_session(self, path, data):
+        """This method validates the current user session given the session token"""
+
         file = open(path, mode='r+')
         tokens = json.loads(file.read())
         data = json.loads(data)
@@ -135,11 +157,15 @@ class Server(BaseHTTPRequestHandler):
         file.close()
 
     def send_token(self, token):
+        """This method sends the session token as part of the HTTP header"""
+
         self.send_response(200)
         self.send_header("Authorization", token)
         self.end_headers()
 
     def resource_not_found(self):
+        """This method responds with HTTP error 404 when a resource is not found"""
+
         self.send_response(404)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
